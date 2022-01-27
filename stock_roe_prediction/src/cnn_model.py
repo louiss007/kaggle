a2a8a39e10c6193137cb9abf6fdddf2fe64c2372
louiss007 <1986zf007@163.com>
2022-01-27 11:06:26 +0800

    add network

---
+++ stock_roe_prediction/src/cnn_model.py
@@ -0,0 +1,9 @@
+"""
+======================
+# -*-coding: utf8-*-
+# @Author  : louiss007
+# @Time    : 22-1-24 下午11:26
+# @FileName: cnn_model.py
+# @Email   : quant_master2000@163.com
+======================
+"""
--- stock_roe_prediction/src/nn_model.py
+++ stock_roe_prediction/src/nn_model.py
@@ -2,8 +2,8 @@
 ======================
 # -*-coding: utf8-*-
 # @Author  : louiss007
-# @Time    : 22-1-23 下午9:00
-# @FileName: rnn_model.py
+# @Time    : 22-1-24 下午11:28
+# @FileName: nn_model.py
 # @Email   : quant_master2000@163.com
 ======================
 """
