9efbe2d883aa28d80c3d26f5c69469ca9ba5a717
louiss007 <louiss007@163.com>
2022-02-07 21:48:15 +0800

    add code for nlp

---
+++ stock_roe_prediction/tf_record/nlp/ner/text_to_tfrecord_for_ner.py
@@ -0,0 +1,17 @@
+"""
+==========================
+# -*- coding: utf8 -*-
+# @Author   : louiss007
+# @Time     : 2022/2/7 21:44
+# @FileName : text_to_tfrecord_for_ner.py
+# @Email    : quant_master2000@163.com
+==========================
+"""
+
+
+def text_to_tfrecord_for_ner():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ stock_roe_prediction/tf_record/nlp/text_class/text_to_tfrecord_for_tc.py
@@ -0,0 +1,17 @@
+"""
+==========================
+# -*- coding: utf8 -*-
+# @Author   : louiss007
+# @Time     : 2022/2/7 21:44
+# @FileName : text_to_tfrecord_for_tc.py
+# @Email    : quant_master2000@163.com
+==========================
+"""
+
+
+def text_to_tfrecord_for_tc():
+    pass
+
+
+if __name__ == '__main__':
+    pass
